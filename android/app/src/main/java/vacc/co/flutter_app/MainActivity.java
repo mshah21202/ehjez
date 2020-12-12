ebc0023e7c8e3b4052c15ff11e220058c60cd8c0
Abood Shahin <tim@timrich.org>
2020-12-12 21:14:41 +0200

    First Commit

---
+++ android/app/src/main/java/vacc/co/flutter_app/MainActivity.java
@@ -0,0 +1,6 @@
+package vacc.co.flutter_app;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+
+
+      home: Scaffold(
+      appBar: AppBar(
+   title: Text("hello"),
+      ),
+ 
+
+
+ body: Center(
+     child: Text("hi")),
+
+
+floatingActionButton: FloatingActionButton(
+  child: Text("test") ,),
+
+
+
+
+    )
+
+
+
+
+  ));
+}
+
