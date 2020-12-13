ee801de8c9104b8dd4a85577ac946f1c869c66cf
Abood Shahin <tim@timrich.org>
2020-12-13 19:19:55 +0200

    latest version

--- lib/mainpage.dart
+++ lib/mainpage.dart
@@ -12,7 +12,7 @@ class mainscreen extends StatelessWidget {
         backgroundColor: Colors.transparent,
         extendBodyBehindAppBar:true,
         appBar: AppBar(
-          title: Center(child: Image.network("")),
+          title: Center(child: Image.asset("assets/Ehjez-Logo.png", width:65 ,height: 65,)),
           elevation: 0.0,
           backgroundColor: Colors.grey[900].withOpacity(0.33),
         ),
@@ -24,14 +24,21 @@ class mainscreen extends StatelessWidget {
                      end: Alignment.topRight,
                      colors: [Color.fromRGBO(31, 112, 138, 1), Color.fromRGBO(64,162,117, 1)],
                  ),),
-             child:Center(
-             child: Text(' احجز لحماية أسرتك', style: TextStyle(fontSize:54.0),
-             )  ),
-               alignment: Alignment.topRight,
+             child: Container( alignment: Alignment(0.9,-0.8),
+             child: Text('                                                              احجز                لحماية أسرتك',
+               textAlign: TextAlign.right,
+               style: TextStyle(
+               fontSize:50.0,
+               fontWeight: FontWeight.bold,
+               color: Colors.white
+                ),
+             ),
+
+
 
            )
       ),
-    ],
+         )],
        ),
     ),
 
