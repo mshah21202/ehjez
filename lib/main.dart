103a44abf022fab03bd05e580c26c49c2293b3c7
Abood Shahin <tim@timrich.org>
2020-12-13 03:40:39 +0200

    updated 3:40 am

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 void main() => runApp(MaterialApp(
   home: splashPage(),
@@ -33,8 +34,41 @@ class splashPage extends StatelessWidget {
           gradient: LinearGradient(
               begin: Alignment.bottomLeft,
               end: Alignment.topRight,
-              colors: [Color.fromRGBO(31, 112, 138, 1), Color.fromRGBO(64,162,117, 1)])
+              colors: [
+                Color.fromRGBO(31, 112, 138, 1),
+                Color.fromRGBO(64, 162, 117, 1)
+              ])
       ),
     ));
+  }
+}
+
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor : Colors.transparent,
+       appBar: AppBar(
+         title: Center(child: Image.asset("")),
+          elevation: 0.0,
+            backgroundColor: Colors.grey[400].withOpacity(0.5),
+    ),
+        body: Container(
+              decoration: BoxDecoration(
+              gradient: LinearGradient(
+              begin: Alignment.bottomLeft,
+              end: Alignment.topRight,
+              colors: [
+               Color.fromRGBO(31, 112, 138, 1),
+               Color.fromRGBO(64, 162, 117, 1)]
+    )
+    )
+        ));
+
+
+
+
+
   }
 }
