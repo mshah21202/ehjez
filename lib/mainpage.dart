c37d8e816c88eec4d0d93fd21b9ef4db85cd4040
Abood Shahin <tim@timrich.org>
2020-12-13 17:01:56 +0200

    latest version

--- lib/mainpage.dart
+++ lib/mainpage.dart
@@ -1,8 +1,42 @@
 import 'package:flutter/material.dart';
 
+
+
 class mainscreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
-  }
-}
+    return MaterialApp(
+     home: Container(
+       child: Stack(
+         children: <Widget>[ Scaffold(
+        backgroundColor: Colors.transparent,
+        extendBodyBehindAppBar:true,
+        appBar: AppBar(
+          title: Center(child: Image.network("")),
+          elevation: 0.0,
+          backgroundColor: Colors.grey[900].withOpacity(0.33),
+        ),
+           body: new Container(
+
+             decoration: BoxDecoration(
+                 gradient: LinearGradient(
+                     begin: Alignment.bottomLeft,
+                     end: Alignment.topRight,
+                     colors: [Color.fromRGBO(31, 112, 138, 1), Color.fromRGBO(64,162,117, 1)],
+                 ),),
+             child:Center(
+             child: Text(' احجز لحماية أسرتك', style: TextStyle(fontSize:54.0),
+             )  ),
+               alignment: Alignment.topRight,
+
+           )
+      ),
+    ],
+       ),
+    ),
+
+     );
+  }}
+
+
+
