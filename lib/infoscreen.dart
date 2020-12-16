24ca9faae137a7d171e5149c19d386e6c3009bdd
Abood Shahin <tim@timrich.org>
2020-12-17 00:05:21 +0200

    latest

--- lib/infoscreen.dart
+++ lib/infoscreen.dart
@@ -6,7 +6,9 @@ class infoscreen  extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
+        body: SingleChildScrollView(child: Container(
+          height:1500 ,
+      width: MediaQuery.of(context).size.width,
     child: Center(
     child: new Container(
     width: double.infinity,
@@ -20,7 +22,46 @@ class infoscreen  extends StatelessWidget {
     Color.fromRGBO(64, 162, 117, 1)
     ],
     ),),
-    ), ),),);
+
+       child: Column( children: <Widget>[
+         Container(padding: const EdgeInsets.fromLTRB(5,40, 8, 0),
+    child: Text(
+    ' عن برنامج إحجز',
+    textAlign: TextAlign.left,
+    style: TextStyle(decoration: TextDecoration.underline, decorationThickness: 2,fontStyle: FontStyle.italic,
+    fontSize: 22.0,
+    fontWeight: FontWeight.bold,
+    color: Colors.white,
+    fontFamily: "ElMessiri",
+                  ),
+                )),
+
+
+
+   Container( padding: const EdgeInsets.fromLTRB(5, 0, 8, 10),
+
+     child: Text(" ابتكر تطبيق احجز في سبيل تسهيل الحصول على لقاح كورونا عبر الهواتف الذكية, وذلك من خلال حجز مواعيد في المراكز الصحية والمستشفيات المعتمدة لإعطاء اللقاح، ويتميز هذا التطبيق بسهولة وسرعة الاستخدام وحفظ خصوصية المستخدم، والأهم من ذلك، فإن التطبيق لا يتطلب من المستخدم إنشاء حساب لتسجيل الدخول،"
+       ,textAlign: TextAlign.right,style: TextStyle(
+           fontSize: 19.0,
+           fontWeight: FontWeight.bold,
+           color: Colors.white,
+           fontFamily: "ElMessiri",)  ),),
+    ]) ),)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    )));
 
 
   }}
