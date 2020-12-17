2d9f38810d2f8d99667a7a067f612cc6bb0348d0
Mohamad Shahin <mohamad.shahin2002@gmail.com>
2020-12-17 16:26:17 +0200

    Finished surveypage2.dart, final touches left

--- lib/surveyPage2.dart
+++ lib/surveyPage2.dart
@@ -10,16 +10,27 @@ class surveyPageSec extends StatefulWidget {
 
 class _surveyPageSecState extends State<surveyPageSec> {
 
-  int groupValue = 0;
-  void group (int e){
-    print(e);
-    setState(() {
-      groupValue = e;
-    });
-
-  }
+List<Map> questionList = [];
+Map<String,String> answer={};
+List<String> answers=['لا','نعم'];
 
   @override
+  void initState(){
+    super.initState();
+    getData();
+  }
+
+  getData(){
+    questionList.add({
+      'id': 'ID1',
+      'question': 'هل لديك أمراض مزمنة؟'
+    });
+    questionList.add({
+      'id': 'ID2',
+      'question': 'هل خضعت لعملية كبرى مؤخراً؟'
+    });
+  }
+
   Widget build(BuildContext context) {
 
     return Scaffold(
@@ -60,39 +71,68 @@ class _surveyPageSecState extends State<surveyPageSec> {
                     ),
                   ),
                     Center(
-                      child: SingleChildScrollView(
-                        child: Column(
-                          children: [Padding(
-                            padding: const EdgeInsets.fromLTRB(0, 0, 45, 0),
-                            child: Row(mainAxisAlignment: MainAxisAlignment.end,
+                      child: Column(
+                        children: [Container(
+                          child: ListView.builder(
+                            shrinkWrap: true,
+                            scrollDirection: Axis.vertical,
+                            itemCount: questionList.length,
+                            itemBuilder: (context, index) {return Column(
                               children: [
-                                Text("هل لديك أمراض مزمنة؟", textAlign: TextAlign.end, style: TextStyle(
-                                  color: Colors.white,
-                                )
-                                  ,)],
+                                Center(
+                                  child: ListTile(
+                                    title: Text( questionList[index]['question'], textAlign: TextAlign.end, style: TextStyle(
+                                      color: Colors.white,
+                                    )
+                                      ,),
+                                    subtitle: Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Theme(data: ThemeData.dark(),
+                                        child: Row(
+                                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                          children: answers.map((s){
+                                            return Row(
+                                              children: <Widget>[
+                                                Text(s,style:TextStyle(color:Colors.white)),
+                                                Radio(
+                                                  activeColor: Colors.white,
+                                                  groupValue: answer[questionList[index]['id']],
+                                                  value: s,
+                                                  onChanged: (newValue) {
+                                                    setState((){
+                                                      answer[questionList[index]['id']]=newValue;
+                                                    });
+                                                  },
+                                                ),
+                                              ],
+                                            );
+                                          }).toList(),
+                                        ),
+                                      ),
+                                    )
+                                  ),
+                                ),
+                              ],
+                            );}
+                          ),
+                        ),
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(40, 40, 40, 40),
+                            child: ButtonTheme(shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadiusDirectional.circular(20)
+                            ),
+                              child: RaisedButton(onPressed: () {Navigator.pushNamed(context, '/survey2');}, color: Color.fromRGBO(46,168,172, 1),
+                                child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                                  Text("التالي", style: TextStyle(color: Colors.white, fontFamily: "ElMessiri", fontSize: 25),),
+                                  Padding(
+                                    padding: const EdgeInsets.fromLTRB(20, 20, 0, 20),
+                                    child: Icon(Icons.arrow_forward_outlined, color: Colors.white,),
+                                  )
+                                ],)
+                                ,),
                             ),
                           ),
-                            Row(
-                              children: [Radio(value: 1, groupValue: null, onChanged: null)],
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.fromLTRB(40, 40, 40, 40),
-                              child: ButtonTheme(shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadiusDirectional.circular(20)
-                              ),
-                                child: RaisedButton(onPressed: () {Navigator.pushNamed(context, '/survey2');}, color: Color.fromRGBO(46,168,172, 1),
-                                  child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-                                    Text("التالي", style: TextStyle(color: Colors.white, fontFamily: "ElMessiri", fontSize: 25),),
-                                    Padding(
-                                      padding: const EdgeInsets.fromLTRB(20, 20, 0, 20),
-                                      child: Icon(Icons.arrow_forward_outlined, color: Colors.white,),
-                                    )
-                                  ],)
-                                  ,),
-                              ),
-                            ),
-                          ],
-                        ),
+                        ],
                       ),
                     )
                   ],
@@ -103,4 +143,3 @@ class _surveyPageSecState extends State<surveyPageSec> {
     );
   }
 }
-
