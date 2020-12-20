acc82b9cff11de11f5a7a6c17e2bf5ecf03e8a34
Abood Shahin <tim@timrich.org>
2020-12-20 22:20:09 +0200

    feedback page needs other designs

--- lib/feedback.dart
+++ lib/feedback.dart
@@ -5,7 +5,7 @@ class feedback extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(backgroundColor: Colors.teal[300],
-    body: SingleChildScrollView(child: SafeArea(
+    body: SafeArea(
     child: Container(
     height:1500 ,
     width: MediaQuery.of(context).size.width,
@@ -25,7 +25,8 @@ class feedback extends StatelessWidget {
                        child: Column(
                          children:[
                            Row(mainAxisAlignment: MainAxisAlignment.start,
-                           children: [Padding(
+                           children: [
+                             Padding(
                              padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
                              child: IconButton(
                                  icon: Icon(Icons.arrow_back_ios_outlined),
@@ -36,14 +37,72 @@ class feedback extends StatelessWidget {
                              ),
                            ),
 
-                           ],),
-                           Padding(
-                               padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+                             Padding(
+                               padding: const EdgeInsets.fromLTRB(32, 25, 0, 0),
                                child: Text("ارسل ملاحظة",style: TextStyle(
-                                   fontSize: 20,
-                                   fontWeight: FontWeight.bold
+                                   color: Colors.white,
+                                   fontSize: 25,
+                                   fontWeight: FontWeight.bold,
+                                   fontFamily: "ElMessiri"
                                ),
                                ),
+                             ),
+                           ],),Container( padding: const EdgeInsets.fromLTRB(20, 27, 20, 20),
+                               child: TextField(maxLines: 1,
+                                 decoration: InputDecoration(
+                                   enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.teal[500]),
+                                       borderRadius: BorderRadius.all(Radius.circular(8.0))),
+                                   focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.white),
+                                       borderRadius: BorderRadius.all(Radius.circular(8.0))),
+                                   errorBorder:OutlineInputBorder(borderSide: BorderSide(color: Colors.white),
+                                       borderRadius: BorderRadius.all(Radius.circular(8.0))) ,
+                                   disabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.white),
+                                       borderRadius: BorderRadius.all(Radius.circular(8.0))),
+
+                                   hintText: "العنوان",
+                                   filled: true,
+                                   fillColor: Colors.teal[400],
+
+
+                                 ),)
+
+                           ),
+                          Container( padding: const EdgeInsets.fromLTRB(20, 0, 20, 20),
+                              child: TextField(maxLines: 10,
+                                decoration: InputDecoration(
+                                enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.teal[500]),
+                                  borderRadius: BorderRadius.all(Radius.circular(8.0))),
+                                  focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.white),
+                                      borderRadius: BorderRadius.all(Radius.circular(8.0))),
+                                  errorBorder:OutlineInputBorder(borderSide: BorderSide(color: Colors.white),
+                                      borderRadius: BorderRadius.all(Radius.circular(8.0))) ,
+                                  disabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.white),
+                                      borderRadius: BorderRadius.all(Radius.circular(8.0))),
+
+                                  hintText: "أكتب ملاحظتك هنا",
+                                    filled: true,
+                                    fillColor: Colors.teal[400],
+
+
+                              ),),),
+
+                           Padding(
+                             padding: const EdgeInsets.fromLTRB(40, 40, 40, 40),
+                             child: ButtonTheme(height:50,
+                               shape: RoundedRectangleBorder(
+                                 borderRadius: BorderRadiusDirectional.circular(20)
+                             ),
+                               child: RaisedButton(onPressed: () {}
+
+                               , color: Color.fromRGBO(46,168,172, 1),
+                                 child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                                   Text("ارسل", style: TextStyle(color: Colors.white, fontFamily: "ElMessiri", fontSize: 25),),
+
+
+
+                                 ],)
+                                 ,),
+                             ),
                            ),
 
 
@@ -51,7 +110,7 @@ class feedback extends StatelessWidget {
 
 
 
-    ]),
+    ],
     ))
 
     ))));
